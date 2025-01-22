5f7fa0dee916678e188b575de79ef7c0e5d78ed3
outquito <out.quito@outlook.com>
2025-01-22 20:12:49 +0200

    add 47

---
+++ Terms.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1. Hello, Go")
+	fmt.Println("2. Hello, \nGo")
+	fmt.Println("3. Hello, \tGo")
+	fmt.Println("4. Hello, \"Go\"")
+	fmt.Println("5. Hello, Go \\")
+}
---
+++ runes.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println('A')
+	fmt.Println('a')
+	fmt.Println('>')
+	fmt.Println('Ю')
+	fmt.Println('\n')
+}
