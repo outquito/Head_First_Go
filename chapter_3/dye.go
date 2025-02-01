f3fd8e80384ceda6a4f6a24d4963499443dcc078
outquito <out.quito@outlook.com>
2025-02-01 20:40:57 +0200

    page 135

--- chapter_3/dye.go
+++ chapter_3/dye.go
@@ -1,16 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-func paintNeeded(width, height float64) float64 {
+func paintNeeded(width, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("a width of %0.2f is invalid", width)
+	}
+	if height < 0 {
+		return 0, fmt.Errorf("a height of %0.2f is invalid", height)
+	}
+
 	area := width * height
 	liters := area / 10.0
 	fmt.Printf("%.2f liters needed\n", liters)
-	return liters
+	return liters, nil
 }
 
 func main() {
-	paintNeeded(4.3, 3.0)
-	paintNeeded(5.2, 3.5)
-	paintNeeded(5.0, 3.3)
+	amount, err := paintNeeded(4.2, -3.0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%0.2f liters needed\n", amount)
+	/*
+		fmt.Println(err)
+		fmt.Printf("%0.2f liters needed\n", amount)
+		/*
+		   total := 0.0
+		   total += paintNeeded(4.3, 3.0)
+		   total += paintNeeded(5.2, 3.5)
+		   total += paintNeeded(5.0, 3.3)
+
+		   fmt.Println("Total liters:", total)
+	*/
 }
