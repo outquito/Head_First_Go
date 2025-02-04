c5cda116f2b90a21506568d8c029b89617d1a5a1
outquito <out.quito@outlook.com>
2025-02-04 17:38:45 +0200

    page 140

--- chapter_3/index.go
+++ chapter_3/index.go
@@ -7,5 +7,11 @@ func main() {
 	var myIntPointer *int
 	myInt = 42
 	myIntPointer = &myInt
-	fmt.Println(*myIntPointer)
+	fmt.Println("1. myIntPointer", myIntPointer)
+	fmt.Println("2. myIntPointer", *myIntPointer)
+	fmt.Println("3. myIntPointer", &myIntPointer)
+	fmt.Println("4. myInt", myInt)
+	//fmt.Println("5. myInt", *myInt)
+	fmt.Println("6. myInt", &myInt)
+
 }
