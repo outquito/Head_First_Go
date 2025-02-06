95585c5d8f0a7da554e1284a668cf287a87e9122
outquito <out.quito@outlook.com>
2025-02-06 14:38:49 +0200

    page 155

---
+++ chapter_4/go/src/go.mod
@@ -0,0 +1,3 @@
+module Head_First_Go/chapter_4/go/src
+
+go 1.23.4
---
+++ chapter_4/go/src/hi/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "Head_First_Go/chapter_4/go/src/greeting"
+
+func main() {
+	greeting.Hello()
+	greeting.Hi()
+}
