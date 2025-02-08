e818413bfeb648c5d7566348d93b9322246f4749
outquito <out.quito@outlook.com>
2025-02-08 20:34:16 +0200

    page 181

--- chapter_4/go/src/hi/main.go
+++ chapter_4/go/src/hi/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "Head_First_Go/chapter_4/go/src/greeting"
+import (
+	"Head_First_Go/chapter_4/go/src/greeting"
+	"Head_First_Go/chapter_4/go/src/greeting/deutsch"
+)
 
 func main() {
 	greeting.Hello()
 	greeting.Hi()
+	deutsch.Hallo()
+	deutsch.GutenTag()
 }
